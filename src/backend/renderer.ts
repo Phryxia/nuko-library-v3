00e6047b5733ebc682ced454dc9c436fe4f7d1e4
phryxia <xahhaepica@gmail.com>
2022-09-08 20:20:32 +0900

    Fix #16 - hydrate id on ssg phase and remove csr hydration

--- src/backend/renderer.ts
+++ src/backend/renderer.ts
@@ -1,7 +1,27 @@
 import type { ShowdownExtension } from 'showdown'
 
+const Counter = {} as Record<string, number>
+
+function removeTags(input: string): string {
+  return input.replace(/<[^>]+>/g, ' ')
+}
+
+const headingRegExp = /<(h\d)(?:[^>]*)>([^<]*)<\/h\d[^>]*>/
+
 export const HeadingAnchorExt: ShowdownExtension = {
   type: 'output',
-  regex: /(<h\d[^>]*>)/g,
-  replace: '$1<a>§</a>',
+  regex: /<h\d[^>]*>[^<]*<\/h\d[^>]*>/g,
+  replace(s: string) {
+    const match = headingRegExp.exec(s)
+
+    if (!match) return s
+
+    const tag = match[1]
+    const innerHtml = match[2]
+    const content = encodeURIComponent(removeTags(innerHtml))
+    const dup = (Counter[content] ??= (Counter[content] ?? -1) + 1)
+    const id = `${content}-${dup}`
+
+    return `<${tag} id="${id}"><a href="#${id}">§</a>${innerHtml}</${tag}>`
+  },
 }
