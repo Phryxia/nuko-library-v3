630ab9f98a1381c9d99c8578444a1edc5888987e
Phryxia <xahhaepica@gmail.com>
2022-09-24 00:24:03 +0900

    Fix #36 (#44)
    * Fix #36 - Fix header nav's maximum height as static
    
    * Fix #36 - Fix header nav font is not set
    
    * Fix #36 - Scroll to selected node when nav is open

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import styles from '@src/styles/Header.module.css'
 import classNames from 'classnames/bind'
 const cx = classNames.bind(styles)
 
-import { Ref, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import Navigator, { NavigatorProps } from './navigator'
 
 interface HeaderProps {
@@ -11,23 +11,19 @@ interface HeaderProps {
   tree: NavigatorProps
 }
 
-const NAV_CONTAINER_PADDING = 15
-
 export default function Header({ title, date, tree }: HeaderProps) {
   const [isNavOpen, setIsNavOpen] = useState<boolean>(false)
-  const [height, setHeight] = useState<number>(0)
-  const navDomRef = useRef<HTMLElement>()
+  const ref = useRef<HTMLElement>(null)
 
   function handleNavOpenClick() {
     setIsNavOpen(!isNavOpen)
+  }
 
-    if (!navDomRef.current) return
+  function handleTransitionEnd() {
+    if (!isNavOpen || !ref.current) return
 
-    if (isNavOpen) {
-      setHeight(0)
-    } else {
-      setHeight(navDomRef.current.clientHeight + NAV_CONTAINER_PADDING)
-    }
+    // @ts-ignore
+    ref.current.scrollToSelected()
   }
 
   return (
@@ -37,13 +33,10 @@ export default function Header({ title, date, tree }: HeaderProps) {
       </a>
       <h2>Last updated on {date}</h2>
       <div
-        className={cx('nav-container')}
-        style={{
-          height: `${height}px`,
-          paddingBottom: isNavOpen ? `${NAV_CONTAINER_PADDING}px` : '0',
-        }}
+        className={cx('nav-container', { open: isNavOpen })}
+        onTransitionEnd={handleTransitionEnd}
       >
-        <Navigator {...tree} ref={navDomRef as Ref<HTMLElement>} />
+        <Navigator {...tree} ref={ref} />
       </div>
       <button className={cx('nav-button')} onClick={handleNavOpenClick}>
         <div className={cx('arrow', { open: isNavOpen })}></div>
