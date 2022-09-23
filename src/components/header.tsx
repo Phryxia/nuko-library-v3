222654eda2f62e9987eab4a9ea2462ea9ec003e7
gfnuko <xahhaepica@gmail.com>
2022-09-24 00:21:18 +0900

    Fix #36 - Scroll to selected node when nav is open

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import styles from '@src/styles/Header.module.css'
 import classNames from 'classnames/bind'
 const cx = classNames.bind(styles)
 
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Navigator, { NavigatorProps } from './navigator'
 
 interface HeaderProps {
@@ -13,9 +13,17 @@ interface HeaderProps {
 
 export default function Header({ title, date, tree }: HeaderProps) {
   const [isNavOpen, setIsNavOpen] = useState<boolean>(false)
+  const ref = useRef<HTMLElement>(null)
 
   function handleNavOpenClick() {
     setIsNavOpen(!isNavOpen)
+  }
+
+  function handleTransitionEnd() {
+    if (!isNavOpen || !ref.current) return
+
+    // @ts-ignore
+    ref.current.scrollToSelected()
   }
 
   return (
@@ -24,8 +32,11 @@ export default function Header({ title, date, tree }: HeaderProps) {
         {title}
       </a>
       <h2>Last updated on {date}</h2>
-      <div className={cx('nav-container', { open: isNavOpen })}>
-        <Navigator {...tree} />
+      <div
+        className={cx('nav-container', { open: isNavOpen })}
+        onTransitionEnd={handleTransitionEnd}
+      >
+        <Navigator {...tree} ref={ref} />
       </div>
       <button className={cx('nav-button')} onClick={handleNavOpenClick}>
         <div className={cx('arrow', { open: isNavOpen })}></div>
--- src/components/navigator.tsx
+++ src/components/navigator.tsx
@@ -2,7 +2,7 @@ import styles from '@src/styles/Linker.module.css'
 import classNames from 'classnames/bind'
 const cx = classNames.bind(styles)
 
-import { forwardRef, Ref, useState } from 'react'
+import { ForwardedRef, forwardRef, Ref, useImperativeHandle, useRef, useState } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 
@@ -15,14 +15,25 @@ export interface NavigatorProps {
   childs: NavigatorProps[]
 }
 
-function NavigatorNode({ title, path, childs = [] }: NavigatorProps) {
+function _NavigatorNode({ title, path, childs = [] }: NavigatorProps, ref: ForwardedRef<any>) {
   const router = useRouter()
   const currentPath = router.asPath.split('#')[0].split('?')[0]
+  const dom = useRef<HTMLDivElement>(null)
 
   // 현재 열린 포스트를 표시할 수 있도록 모든 폴더의 열림 기본값을 설정한다.
   // 예컨데 라우터 경로가 /babo/a/c이고 현재 링커의 고유경로가 /babo/a이면 이 링커는 열려야함
   const [isOpen, setIsOpen] = useState<boolean>(currentPath.indexOf(path) === 0)
   const isSelected = currentPath === path
+
+  useImperativeHandle(ref, () => ({
+    scrollToSelected() {
+      if (isSelected && dom.current) {
+        dom.current.parentElement?.scrollTo({
+          top: dom.current.offsetTop,
+        })
+      }
+    },
+  }))
 
   function handleClick() {
     setIsOpen(!isOpen)
@@ -30,7 +41,7 @@ function NavigatorNode({ title, path, childs = [] }: NavigatorProps) {
 
   const isFolder = childs.length > 0
   return (
-    <div className={cx('linker')}>
+    <div className={cx('linker')} ref={dom}>
       {isFolder ? (
         <>
           <button className={cx('linker_row')} onClick={handleClick}>
@@ -42,7 +53,7 @@ function NavigatorNode({ title, path, childs = [] }: NavigatorProps) {
           {isOpen && (
             <div className={cx('children')}>
               {childs.map((child) => (
-                <NavigatorNode key={child.path} {...child} />
+                <NavigatorNode key={child.path} {...child} ref={ref} />
               ))}
             </div>
           )}
@@ -58,10 +69,12 @@ function NavigatorNode({ title, path, childs = [] }: NavigatorProps) {
   )
 }
 
+const NavigatorNode = forwardRef(_NavigatorNode)
+
 export default forwardRef(function Navigator(props: NavigatorProps, ref: Ref<HTMLElement>) {
   return (
-    <nav ref={ref}>
-      <NavigatorNode {...props} />
+    <nav>
+      <NavigatorNode {...props} ref={ref} />
     </nav>
   )
 })
